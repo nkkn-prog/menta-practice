f3e0e884cca9c54c1f755c1e6abb719d212f8b3a
Naoki Ando <nkkn@NaokinoMacBook-Pro.local>
2024-11-19 20:59:58 +0900

    add axois version

--- js-practice/async-practice.js
+++ js-practice/async-practice.js
@@ -1,10 +1,30 @@
 const getUrl = 'https://jsonplaceholder.typicode.com/photos'
+const axios = require('axios');
 
 // async, await, axiosを使ったやり方
-const fetchData = async () => {
-    const data = await fetch(getUrl)
-    const json = await data.json()
-    console.log(json)
-}
+// const fetchData = async () => {
+//     const data = await fetch(getUrl)
+//     const json = await data.json()
+//     console.log(json)
+// }
 
+// fetchを使ったやり方
+// fetch('https://jsonplaceholder.typicode.com/photos')
+//     .then(response => response.json())
+//     .then(postDate => console.log(postDate.slice(0,50)))
+
+
+// fetchData()
+
+// axiosを使ったやり方
+const fetchData = async() => {
+    try{
+        const response = await axios.get(getUrl)
+        console.log(response)
+        console.log(response.status)
+    } catch(error) {
+        console.log(error.message)
+    }
+
+}
 fetchData()
--- js-practice/fetch-practice.js
+++
@@ -1,4 +0,0 @@
-// fetchを使ったやり方
-fetch('https://jsonplaceholder.typicode.com/photos')
-    .then(response => response.json())
-    .then(postDate => console.log(postDate.slice(0,50)))
