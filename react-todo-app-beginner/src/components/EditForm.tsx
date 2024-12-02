8abd52e6f1277447fd51774edffb1cb348948309
nkkn-prog <nkkn.prog@gmail.com>
2024-12-02 19:47:00 +0900

    add style

--- react-todo-app-beginner/src/components/EditForm.tsx
+++ react-todo-app-beginner/src/components/EditForm.tsx
@@ -1,14 +1,15 @@
  export const EditForm = (props) => {
 
-    
-    <>
-      <input
-        type='text'
-        label='新しいタイトル'
-        value={newTitle}
-        onChange={handleEditFormChanges}
-      />
-      <button onClick={()=>handleEditTodo}>編集を保存</button>
-      <button onClick={()=>handleCloseEditForm}>キャンセル</button>
-    </>;
+    return (
+        <>
+            <input
+                type='text'
+                label='新しいタイトル'
+                value={newTitle}
+                onChange={handleEditFormChanges}
+            />
+            <button onClick={()=>handleEditTodo}>編集を保存</button>
+            <button onClick={()=>handleCloseEditForm}>キャンセル</button>
+        </>
+    )
   };
